1709bd0bdd9c3863d5e1806614cd7a5bbc634a86
Venkata Sivaprasad Reddy Pulagam <pvsivapr@gmail.com>
2022-04-06 12:55:20 +0530

    First commit to publish raw data to all the clients

---
+++ ProjFiles/SignalRServer/Server.js
@@ -0,0 +1,26 @@
+var express = require('express');// Importing Express
+var SignalRJS = require('signalrjs');// Importing SignalR
+
+
+var app = express();//Getting App From Express
+var signalR = SignalRJS();
+
+const port = process.env.PORT || 3000;//8080;//Creating A Constant For Providing The Port
+const timeInterval = 1000;
+
+app.use(signalR.createListener());
+app.use(express.static(__dirname));
+app.listen(port);
+
+
+signalR.on('CONNECTED', function(){
+    console.log('connected to signalR');
+    setInterval(function(){
+        signalR.send(
+            {
+                time: new Date()
+            }
+        );
+    }, timeInterval)
+});
+console.log("Server Running At:localhost:"+port);
